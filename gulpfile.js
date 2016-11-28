567180ffe8dea902b7430dd16bc0bcc31154d12f
Matt Lane <mlane@appnexus.com>
2016-11-28 10:36:56 -0800

    Integrate Browserstack tests into Travis CI build
    Resolves #766

--- gulpfile.js
+++ gulpfile.js
@@ -92,7 +92,10 @@ gulp.task('test', function () {
   var browserArgs = helpers.parseBrowserArgs(argv).map(helpers.toCapitalCase);
 
   if (process.env.TRAVIS) {
-    browserArgs = ['Chrome_travis_ci'];
+    browserArgs = [
+      'bs_chrome_51_mac_elcapitan',
+      'bs_firefox_46_windows_10',
+    ];
   }
 
   if (argv.browserstack) {
